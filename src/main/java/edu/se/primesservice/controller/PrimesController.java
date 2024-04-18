2ff9a9d47035bc9cb32ce3139275fa2380bebe46
bdokala <bdokala@iu.edu>
2024-04-18 01:33:43 -0400

    adding practicum 13 files

--- src/main/java/edu/se/primesservice/controller/PrimesController.java
+++ src/main/java/edu/se/primesservice/controller/PrimesController.java
@@ -4,6 +4,8 @@ package edu.se.primesservice.controller;
 import ch.qos.logback.classic.spi.ConfiguratorRank;
 import edu.se.primesservice.rabbitmq.MQSender;
 import edu.se.primesservice.service.IPrimesService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,7 +25,11 @@ public class PrimesController {
     @GetMapping("/{n}")
     public boolean isPrime(@PathVariable int n){
         boolean result = iPrimesService.isPrime(n);
-        mqSender.sendMessage(n,result);
-        return iPrimesService.isPrime(n);
+        Object principal = SecurityContextHolder
+                .getContext().getAuthentication().getPrincipal();
+        String username = ((Jwt) principal).getSubject();
+        System.out.println(username);
+        mqSender.sendMessage(username, n,result);
+        return result;
     }
 }
